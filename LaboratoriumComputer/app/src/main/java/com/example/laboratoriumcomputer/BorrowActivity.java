2874dd931f3e97666dab23527b3878a753b418bb
Ian M. <amazeianproxy@gmail.com>
2025-11-26 22:13:09 +0700

    Baseline + Title + Menu

---
+++ LaboratoriumComputer/app/src/main/java/com/example/laboratoriumcomputer/BorrowActivity.java
@@ -0,0 +1,52 @@
+package com.example.laboratoriumcomputer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class BorrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_borrow);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ImageButton menuButton = findViewById(R.id.menu_button);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        menuButton.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_dashboard) {
+                startActivity(new Intent(BorrowActivity.this, MainActivity.class));
+            } else if (id == R.id.menu_inventory) {
+                startActivity(new Intent(BorrowActivity.this, InventoryActivity.class));
+            } else if (id == R.id.menu_return) {
+                startActivity(new Intent(BorrowActivity.this, ReturnActivity.class));
+            } else if (id == R.id.menu_history) {
+                startActivity(new Intent(BorrowActivity.this, HistoryActivity.class));
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
